a7766888655e42a8ff7dc5a766ea6f96c73eed88
lingxu <lingx_1@chinatelecom.cn>
2023-02-22 14:13:27 +0800

    add diskio, disk input; delete unused plugins

--- plugins/inputs/all/snmp.go
+++
@@ -1,5 +0,0 @@
-//go:build !custom || inputs || inputs.snmp
-
-package all
-
-import _ "github.com/XenoStar123/telegraf/plugins/inputs/snmp" // register plugin
--- plugins/processors/all/ifname.go
+++
@@ -1,5 +0,0 @@
-//go:build !custom || processors || processors.ifname
-
-package all
-
-import _ "github.com/XenoStar123/telegraf/plugins/processors/ifname" // register plugin
